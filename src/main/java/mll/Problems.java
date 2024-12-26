9dc818d28d784e1a25e6483327da3c1af80aaad4
MLL <marclangwieler@gmail.com>
2024-12-26 08:58:53 +0100

    Added Problem 2

--- src/main/java/mll/Problems.java
+++ src/main/java/mll/Problems.java
@@ -1,9 +1,13 @@
 package mll;
 
 import mll.Archive.Problem001;
+import mll.Archive.Problem002;
 
 public interface Problems {
     public static int problem001(){
         return Problem001.calculate();
+    }
+    public static int problem002(){
+        return Problem002.calculate();
     }
 }
