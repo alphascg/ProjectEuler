9b5b5cbde268ce844eb1fcb78f869a5dc37bd654
MLL <marclangwieler@gmail.com>
2024-12-26 09:45:07 +0100

    Added Solution

--- src/main/java/mll/Archive/Problem009.java
+++ src/main/java/mll/Archive/Problem009.java
@@ -2,6 +2,16 @@ package mll.Archive;
 
 public abstract class Problem009 {
     public static int calculate(){
+        int target = 1000;
+        for (int a = 1; a < target/2; a++) {
+            for (int b = 1; b < target/2; b++) {
+                for (int c = 1; c < target; c++) {
+                    if( (a*a)+(b*b)==(c*c) && (a+b+c==target) ){
+                        return a*b*c;
+                    }
+                }
+            }
+        }
         return 0;
     }
 }
