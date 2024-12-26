ef9ed71c318332f48471ab3669449bdeea597bf0
MLL <marclangwieler@gmail.com>
2024-12-26 09:43:33 +0100

    Added Test for Problem 1

--- src/test/java/Tester.java
+++ src/test/java/Tester.java
@@ -3,4 +3,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class Tester {
+    @Test
+    public void test_problem001(){
+        Assertions.assertEquals(Problems.problem001(),233168);
+    }
 }
