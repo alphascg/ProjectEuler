a23be2f24c944194a4a5df9932ee53d13d5d08e7
MLL <marclangwieler@gmail.com>
2024-12-26 09:18:35 +0100

    Refactoring because of renaming

--- src/main/java/mll/Problems.java
+++ src/main/java/mll/Problems.java
@@ -1,13 +1,13 @@
 package mll;
 
 import mll.Archive.Problem001;
-import mll.Archive.Problem002;
+import mll.Archive.Problem006;
 
 public interface Problems {
     public static int problem001(){
         return Problem001.calculate();
     }
     public static int problem002(){
-        return Problem002.calculate();
+        return Problem006.calculate();
     }
 }
