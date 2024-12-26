ed6b17736e45fa5f6a81d8177e962084f0f9f508
MLL <marclangwieler@gmail.com>
2024-12-26 09:09:18 +0100

    Added Solution

--- src/main/java/mll/Archive/Problem002.java
+++ src/main/java/mll/Archive/Problem002.java
@@ -2,6 +2,20 @@ package mll.Archive;
 
 public class Problem002 {
     public static int calculate(){
-        return 0;
+        int sumOfSquares = 0;
+        int amountOfFirstNumbers = 100;
+        for (int i = 1; i <= amountOfFirstNumbers; i++) {
+            sumOfSquares += i*i;
+        }
+
+        int sum = 0;
+        for (int j = 1; j <= amountOfFirstNumbers; j++) {
+            sum +=j;
+        }
+        int squareOfSums=sum*sum;
+
+        int difference = squareOfSums - sumOfSquares;
+
+        return difference;
     }
 }
